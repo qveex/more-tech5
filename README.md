b226b802601ea74993d440b293ed5a0de7fea0b6
qveex <npkololo@yandex.ru>
2023-10-13 18:40:27 +0300

    init

---
+++ README.md
@@ -0,0 +1,2 @@
+# more-tech5-mobile
+MORE.Tech 5.0 mobile app
---
+++ app/src/main/java/qveex/ru/more/MainActivity.kt
@@ -0,0 +1,24 @@
+package qveex.ru.more
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import qveex.ru.more.ui.theme.Moretech5Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Moretech5Theme {
+
+            }
+        }
+    }
+}
