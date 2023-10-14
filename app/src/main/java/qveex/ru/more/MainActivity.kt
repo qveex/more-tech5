7f5551e1da8445ecf6fcd7bf7ebe411eab203b86
mezhendosina <mensh.zheya@gmail.com>
2023-10-14 22:16:46 +0300

    fix: add request permission launcher

--- app/src/main/java/qveex/ru/more/MainActivity.kt
+++ app/src/main/java/qveex/ru/more/MainActivity.kt
@@ -1,8 +1,10 @@
 package qveex.ru.more
 
+import android.Manifest
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
 import com.yandex.mapkit.MapKitFactory
 import dagger.hilt.android.AndroidEntryPoint
 import qveex.ru.more.presentation.screens.MainScreen
@@ -10,11 +12,16 @@ import qveex.ru.more.ui.theme.Moretech5Theme
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    private val requestPermissionLauncher =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) {
+        }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        requestPermissionLauncher.launch(Manifest.permission.ACCESS_COARSE_LOCATION)
         MapKitFactory.setApiKey(BuildConfig.MAPKIT_API_KEY)
-
         setContent {
             Moretech5Theme {
                 MainScreen()
